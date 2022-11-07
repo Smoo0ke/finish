8ab1985e9593c2ca78159462edaf623c85196843
sm000ke <mishadymov2005@yandex.ru>
2022-11-08 00:34:38 +1000

    делаем основной массив

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using System;
 class MainProgramm {
-  static void Main()
+  static void Main() {
+            string[] mass;
+            Console.WriteLine("Введите слова через пробел: ");
+            string words = Console.ReadLine();
+            mass = words.Split(' ');
+            var result = new string[mass.Length];
+            var realSize = 0;
+            foreach (var value in mass )
